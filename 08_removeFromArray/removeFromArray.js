7eda90aacd5817ed2e6dec9a3fccb4381ac8ca60
matt <git@mking.io>
2025-11-07 19:21:52 +0000

    08_removeFromArray: Complete exercise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...elements) {
+    for (const element of elements) {
+        arr = arr.filter(value => value !== element);
+    }
+    return arr;
 };
+
+// Could use .includes() to avoid looping through the elements array.
 
 // Do not edit below this line
 module.exports = removeFromArray;

38c9de4c4d09c925a106335bdc610568b1c45f1c
matt <git@mking.io>
2025-11-07 20:43:48 +0000

    09_sumAll: Complete exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let numPair = [a, b].sort((a, b) => a - b);
 
+    if (
+        numPair[0] < 0 ||
+        numPair.some(item => item % 1 !== 0) ||
+        numPair.some(item => typeof item !== "number")
+    ) return "ERROR";
+
+    let sum = 0;
+    for (let i = numPair[0]; i <= numPair[1]; i++) {
+        sum += i;
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line

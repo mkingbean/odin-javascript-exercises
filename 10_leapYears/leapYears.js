e86df60901d282c4b7909471b86fd39ceb2a9f6c
matt <git@mking.io>
2025-11-07 21:03:09 +0000

    10_leapYears: Complete exercise

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let divisibleBy4 = (year % 4 === 0);
+    let divisibleBy100 = (year % 100 === 0);
+    let divisibleBy400 = (year % 400 === 0);
 
+    if (divisibleBy400) {
+        return true;
+    } else if (divisibleBy4 && !divisibleBy100) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line

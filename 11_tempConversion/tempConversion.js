5dac122a497e5e341d8ae63dadc36db35981dc98
matt <git@mking.io>
2025-11-08 09:15:09 +0000

    11_tempConversion: Complete exercise

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFar) {
+  // Formula: (far - 32) * (5/9)
+  tempCel = (tempFar - 32) * (5/9);
+  return +tempCel.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCel) {
+  // Formula: (cel * 9/5) + 32
+  tempFar = (tempCel * 9/5) + 32;
+  return +tempFar.toFixed(1);
 };
 
 // Do not edit below this line

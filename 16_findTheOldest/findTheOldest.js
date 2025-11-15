acc7480e21e9f9569e2d60976846b27c6db5f889
matt <git@mking.io>
2025-11-15 17:38:25 +0000

    16_findTheOldest: Complete exercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,10 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(people) {
+    const getAge = function getAge(person) {
+        return (person.yearOfDeath ?? new Date().getFullYear()) - person.yearOfBirth;
+    }
+    
+    return people.reduce((currentOldest, person) => getAge(person) > getAge(currentOldest) ? person : currentOldest);
+    };
 
 // Do not edit below this line
 module.exports = findTheOldest;

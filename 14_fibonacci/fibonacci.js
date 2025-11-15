5055ffe1b3f2f203e90afc22551a8f9eca32c012
matt <git@mking.io>
2025-11-15 16:06:04 +0000

    14_fibonacci: Complete exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = +n;
+    if (n < 0) {
+        return "OOPS";
+    } else {
+        const sequence = [0, 1];
+        for (let i = 2; i <= n; i++) {
+            nextTerm = sequence[i - 1] + sequence[i - 2];
+            sequence.push(nextTerm);
+        }
+        return sequence.at(n);
+    }
 };
 
 // Do not edit below this line

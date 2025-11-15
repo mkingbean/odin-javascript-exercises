dc43c8a317e92955cc54f9f38d983be5849a0045
matt <git@mking.io>
2025-11-15 15:48:15 +0000

    13_palindromes: Complete exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+    const validChars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+    let arr = phrase
+    .split("")
+    .filter(char => validChars.includes(char))
+    .map(char => char.toLowerCase());
+
+    let str = arr.join("");
+    let reversedStr = arr.reverse().join("");
+    
+    return str === reversedStr;
 };
 
 // Do not edit below this line
